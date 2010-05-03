5661524f8e3d9b82f7290feb855bc442a4d0baf4
Oregu <oleg.burykin@gmail.com>
2010-05-03 19:07:27 +0400

    indentation from Emacs

--- project-euler/euler_003_biggest_prime_div.rb
+++ project-euler/euler_003_biggest_prime_div.rb
@@ -2,29 +2,28 @@ require "../util/prime"
 
 class PrimeDividerSolver
 
-	NUMBER = 386745374779148463746059
+  NUMBER = 386745374779148463746059
 
-	def solve
-		(3..10).each do |a|
+  def solve
+    (3..10).each do |a|
+      next if NUMBER%a != 0
 
-			next if NUMBER%a != 0
+      puts "Checking #{NUMBER}/#{a}"
+      div = NUMBER/a
 
-			puts "Checking #{NUMBER}/#{a}"
-			div = NUMBER/a
-
-			return div if Prime.is_prime div
-		end
+      return div if Prime.is_prime div
+    end
         
-		0
-	end
+    0
+  end
 end
 
 if __FILE__ == $0
 
-	s = PrimeDividerSolver.new
-	divdr = s.solve
+  s = PrimeDividerSolver.new
+  divdr = s.solve
 
-	puts "No answer" if divdr == 0
-	puts "BPD is #{divdr}" if divdr != 0
-	puts
+  puts "No answer" if divdr == 0
+  puts "BPD is #{divdr}" if divdr != 0
+  puts
 end
