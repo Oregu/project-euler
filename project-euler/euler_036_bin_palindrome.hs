5bf9a6ae6936b3986e6ff37cbc69ab45c9362c63
Oregu <oleg.burykin@gmail.com>
2011-11-03 14:38:08 +0400

    Euler 36 solved

---
+++ project-euler/euler_036_bin_palindrome.hs
@@ -0,0 +1,21 @@
+{-
+  The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+  Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+  (Please note that the palindromic number, in either base, may not include leading zeros.)
+-}
+import Numeric (showIntAtBase)
+import Char (intToDigit)
+
+main = print solve
+
+solve = sum $ filter isDoublePalindromic [1..999999]
+
+isDoublePalindromic n = isPalindrome n && isBinPalindrome n
+
+toBin d = showIntAtBase 2 intToDigit d ""
+
+isBinPalindrome = isStrPalindrome . toBin 
+
+isPalindrome = isStrPalindrome . show
+
+isStrPalindrome s = reverse s == s
