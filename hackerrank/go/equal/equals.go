da3c6b781f19e5b91ee41c1937fe75eeff84c929
Oregu <oleg.burykin@gmail.com>
2017-01-02 20:40:43 -0800

    Final solution. See comments.
    The thing is that we can treat adding to everybody else
    as removing chocolates from that exact intern.
    So we need to count number of reduce operations for each intern.
    
    Took me couple of days to solve.
    Had to look into duscussions section.
    Which I'm not particularly happy about,
    
    Used paper on the last day to finally understand the answer.
    
    Here is a great idea though: SOLVE IT ON PAPER FIRST.
    And only then go to editor.
    
    Solving in editor takes most of the time,
    sometimes with random guesse which makes it even worse.
    
    Full understanding first, then coding.

--- hackerrank/go/equal/equals.go
+++ hackerrank/go/equal/equals.go
@@ -28,6 +28,7 @@ func main() {
 	}
 
 	for i := 0; i < len(data); i++ {
+		// No need to sort, just find smallest during load.
 		sort.Ints(data[i])
 		rounds := searchEqualAlgo(data[i])
 		fmt.Println(rounds)
@@ -37,19 +38,40 @@ func main() {
 func searchEqualAlgo(chocolates []int) int {
 	// Give to everybody except second smallest, until smallest will match.
 	// Continue until biggest.
+
+	// Following solution gives only minimum number of chocolates!
+	// But we want to get shortest number of rounds instead.
+	// So there can be more chocolates in the end, but less steps taken.
+	// E.g. steps(5) == 1, but steps(3) == 2 (2, 1)
+	//
+	// for i := 0; i < len(chocolates)-1; i++ {
+	//     rounds += steps(chocolates[i] - chocolates[0] + shift)
+	// }
+	//
+	// So instead we want to try raising everybody chocolates to
+	// biggest, biggest+1, biggest+2, biggest+3
+
+	minRounds := 1<<31 - 1
+	for i := 0; i <= 4; i++ {
+		if chocolates[0] < i { break }
+		rounds := rounds(chocolates, i)
+		if minRounds > rounds { minRounds = rounds }
+	}
+	return minRounds
+}
+
+func rounds(chocolates []int, shift int) int {
 	rounds := 0
-	differenceSum := 0
-	for i := 0; i < len(chocolates)-1; i++ { 
-		differenceSum += chocolates[i+1] - chocolates[i]
-		rounds += splitRounds(differenceSum)
+	for i := 0; i < len(chocolates); i++ {
+		rounds += steps(chocolates[i] - chocolates[0] + shift)
 	}
 	return rounds
 }
 
-func splitRounds(difference int) int {
+func steps(difference int) int {
 	fives := difference/5
 	fivesLeftover := difference % 5
 	twos := fivesLeftover >> 1
-	twosLeftOver := fivesLeftover % 2
+	twosLeftOver := fivesLeftover & 1
 	return fives + twos + twosLeftOver
 }
